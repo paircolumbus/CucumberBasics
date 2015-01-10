4c77a1cf8403c81cdf7ca405996b39be5cd48a21
Nicolas McCurdy <thenickperson@gmail.com>
2015-01-09 19:33:59 -0500

    Nick's solution

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,14 @@
-#Create your feature here
+Feature: Animal
+  As a hotel worker
+  Given a client who is an animal
+  I want to track the animal and see if it is old
+
+  Scenario: Young Cat
+    Given a 2 year old cat named Garfield
+    Then it is not old
+    And I should see its name, type, and age
+
+  Scenario: Old Dog
+    Given a 5 year old dog named Gromit
+    Then it is old
+    And I should see its name, type, and age
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,14 @@
-#Delete this comment, here is where you should write your step defs
+Given /^a (\d+) year old (\w+) named (\w+)$/ do |age, type, name|
+  @age, @type, @name = age.to_i, type, name
+  @animal = Animal.new(@name, @type, @age)
+end
+
+Then /^it is (old|not old)$/ do |situation|
+  expect(@animal.old?).to be situation == 'old'
+end
+
+And /^I should see its name, type, and age$/ do
+  expect(@animal.name).to be @name
+  expect(@animal.type).to be @type
+  expect(@animal.age).to be @age
+end
