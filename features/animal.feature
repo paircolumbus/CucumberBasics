577cd6330cbbbc6de1448329d1c8b75ce3a11af4
Jonathan Martindell <jmartindell@covermymeds.com>
2015-01-14 15:57:23 -0500

    First solution using cucumber syntax

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,12 @@
-#Create your feature here
+Feature: Animal
+  In order to properly describe animals
+  As a hotel manager
+  I want to know the name, type, age, and whether the animal is older
+
+  Scenario: Cat 
+    Given a 4 year old cat named Lucy
+    Then I should receive Lucy when I ask for its name
+    Then I should receive 4 when I ask for its age 
+    Then I should receive cat when I ask for its type
+    Then I should receive true when I ask if its older
+
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,19 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^a 4 year old cat named Lucy$/) do
+  @animal = Animal.new('Lucy', 'cat', 4)
+end
+
+Then(/^I should receive Lucy when I ask for its name$/) do
+  expect(@animal.name).to eq('Lucy')
+end
+
+Then(/^I should receive cat when I ask for its type$/) do
+  expect(@animal.type).to eq('cat')
+end
+
+Then(/^I should receive 4 when I ask for its age$/) do
+  expect(@animal.age).to be 4
+end
+
+Then(/^I should receive true when I ask if its older$/) do
+  expect(@animal.old?).to be true 
+end
