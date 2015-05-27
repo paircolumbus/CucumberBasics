2514fb6d308481257b371c9671d8fe489b8bdc4d
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-27 11:34:37 -0400

    Well, all my tests pass

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,24 @@
-#Create your feature here
+Feature: Animal
+  As an animal
+  I want my name, type, age, and oldness to be known
+  In order to be treated properly
+
+  Scenario: Animal named Gorbypuff
+    Given a cat
+    When I have a name
+    Then I should be called "Gorbypuff"
+
+  Scenario: Animal of type Roosevelt Elk
+    Given an elk
+    When I have a type
+    Then I should be recognized as a Roosevelt Elk
+
+  Scenario: Animal of age 7
+    Given a dog
+    When I have an age
+    Then I should be of age 7
+
+  Scenario: Elderly animal
+    Given a chincilla
+    When I am older than 3
+    Then I should be an old animal
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,47 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^a cat$/) do
+  @cat = Animal.new('Gorbypuff', 'cat', 3)
+end
+
+When(/^I have a name$/) do
+  expect(@cat.name).not_to be_nil
+end
+
+Then(/^I should be called "(\w+)"$/) do |name|
+  expect(@cat.name).to eq(name)
+end
+
+Given(/^an elk$/) do
+  @elk = Animal.new('George', 'Roosevelt Elk', 6)
+end
+
+When(/^I have a type$/) do
+  expect(@elk.type).not_to be_nil
+end
+
+Then(/^I should be recognized as a (.*)$/) do |type|
+  expect(@elk.type).to eq(type)
+end
+
+Given(/^a dog$/) do
+  @dog = Animal.new('Spot', 'dog', 7)
+end
+
+When(/^I have an age$/) do
+  expect(@dog.age).not_to be_nil
+end
+
+Then(/^I should be of age (\d)$/) do |age|
+  expect(@dog.age).to eq(age.to_i)
+end
+
+Given(/^a chincilla$/) do
+  @chincilla = Animal.new('Fluffy', 'chincilla', 4)
+end
+
+When(/^I am older than (\d)$/) do |age|
+  expect(@chincilla.age).to be > age.to_i
+end
+
+Then(/^I should be an old animal$/) do
+  expect(@chincilla.old?).to eq(true)
+end
