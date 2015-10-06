53ffee74b11e2425aad51b5d8a031a49ec18260a
Gabriel Escamilla <GEscamilla@covermymeds.com>
2015-10-06 11:24:04 -0400

    handles attribute readers

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,17 @@
-#Create your feature here
+Feature: Animal
+  In order to properly address all animals
+  As an animal
+  I want to provide my name, type and age
+  As an animal older than 3 years
+  I want to say I'm old
+
+
+  Scenario: Animal
+    Given an animal
+    When I ask about its "name"
+    Then I should get its "name"
+    When I ask about its "type"
+    Then I should get its "type"
+    When I ask about its "age"
+    Then I should get its "age"
+
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,15 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^an animal$/) do
+  @name = "Any"
+  @type = "animal"
+  @age = 4
+  @attributes = {'name' => @name, 'type' => @type, 'age' => @age}
+  @animal = Animal.new(@name, @type, @age)
+end
+
+When(/^I ask about its "(.*?)"$/) do |attr|
+  expect(@attributes.include?(attr)).to be true
+end
+
+Then(/^I should get its "(.*?)"$/) do |attr|
+  expect(@animal.send(attr)).to eq @attributes[attr]
+end
