bd18e86debd70eb0c86f7aff36f25006f5f6ea9e
Joe Reich <jreich@CMDVMOSXLT126.local>
2015-12-13 12:23:28 -0500

    Looked at other people's solutions

--- features/animal.feature
+++ features/animal.feature
@@ -1,8 +1,10 @@
 Feature: Animal
   In order to properly track animals
-  Every animal 
-  Should have name, type, and age
+  Every animal should have name, type, and age
 
   Scenario:
     Given a 5-year old 'cat' named 'Babushka the Talking Cat'
-    Then it should be tracked as a 'cat', named 'Babushka the Talking Cat', whose age is 5
+    Then it should be tracked as a 'cat'
+    And it should be named 'Babushka the Talking Cat'
+    And its age should be 5
+    And it should count as old
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -2,8 +2,18 @@ Given (/^a (\d+)\-year old '(.*?)' named '(.*?)'$/) do |age, type, name|
   @animal = Animal.new(name, type, age.to_i)
 end
 
-Then(/^it should be tracked as a '(.*?)', named '(.*?)', whose age is (\d+)$/) do |type, name, age|
-  expect(@animal.name).to eq name
+Then (/^it should be tracked as a '(.*?)'$/) do |type|
   expect(@animal.type).to eq type
+end
+
+Then (/^it should be named '(.*?)'$/) do |name|
+  expect(@animal.name).to eq name
+end
+
+Then (/^its age should be (\d+)$/) do |age|
   expect(@animal.age).to eq age.to_i
+end
+
+Then (/^it should count as old$/) do
+  expect(@animal.old?).to be_truthy
 end
