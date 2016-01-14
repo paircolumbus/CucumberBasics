335e5902bb574e6527079e63330aca0569315714
Priya <priya.ramesh6@gmail.com>
2016-01-14 13:04:44 -0500

    Completed

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,11 @@
-#Create your feature here
+
+Feature: Animal
+  In order to properly allocate rooms
+  As a young cat
+  I want a 'Room for young cats'
+  
+  Scenario: Cat
+    Given a animal
+    When younger than 3
+    And I am a "cat"
+    Then I want the room for "Mr Cat"
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,18 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^I am a animal$/) do
+  @greeter = Greeter.new('Lucy', 'cat', 4)
+  @animal = Animal.new('Lucy', 'cat', 4)
+end
+
+When(/^younger than (\d+)$/) do |arg1|
+  expect(@animal.old?).to be true
+end
+
+When(/^I am a "(.*?)"$/) do |type|
+  greeter = Greeter.new(@animal)
+  expect(greeter.animal.type).to eq type
+end
+
+Then(/^I want the room for "(.*?)"$/) do |title|
+  greeter = Greeter.new(@animal)
+  expect(greeter.greet).to eq title
+end
