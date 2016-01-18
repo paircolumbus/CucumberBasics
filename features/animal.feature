51b0e034e09464090d67bf91198dfbf2876a68e7
Austen Madden <maddenausten@gmail.com>
2016-01-18 10:50:53 -0500

    Austen's Solution

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,11 @@
-#Create your feature here
+Feature: Animal
+  In order to properly make animals
+  As a old cat
+  I should be named Lucy
+  
+  Scenario: Lucy
+    Given I am an animal
+    When I am older than 3
+    And I check that I am a "cat"
+    Then my name should be "Lucy"
+
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,15 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^I am an animal$/) do
+  @animal = Animal.new('Lucy', 'cat', 4)
+end
+
+When(/^I am older than (\d+)$/) do |arg1|
+  expect(@animal.old?).to be true 
+end
+
+When(/^I check that I am a "(.*?)"$/) do |type|
+  expect(@animal.type).to eq type
+end
+
+Then(/^my name should be "(.*?)"$/) do |name|
+  expect(@animal.name).to eq name
+end
