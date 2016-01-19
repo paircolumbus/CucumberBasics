d3ed2e3c746a6fc99ac7f91af9abdc429229cca3
marcel <mkooi@covermymeds.com>
2016-01-19 16:06:49 -0500

    wrote and passed tests for animal

--- features/animal.feature
+++ features/animal.feature
@@ -1,6 +1,10 @@
 Feature: Animal
-	In order to 
+	In order to be properly catered to as a Lion
+	As a 5 year old Lion
+	I want to be considered old
 
 
 	Scenario: Lion
-		Given a animal
+		Given an animal that is a Lion
+		When he is 5
+		Then he is old
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,12 @@
 #Delete this comment, here is where you should write your step defs
+Given(/^an animal that is a Lion$/) do
+  @animal = Animal.new('Mike', 'Lion', 5)
+end
+
+When(/^he is (\d+)$/) do |arg1|
+  expect(@animal.age.to_s).to eq arg1
+end
+
+Then(/^he is old$/) do
+  expect(@animal.old?).to eq true
+end
