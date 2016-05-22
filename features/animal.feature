c8bb5d68f3643c7587f7ab99f20c975188bb38cb
janmilosh <janmilosh@gmail.com>
2016-05-21 23:23:31 -0400

    Completes exercise

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,12 @@
-#Create your feature here
+Feature: Clientele tracker
+  As a hotel manager
+  I want to track my animal clientele
+  
+  Scenario: Old animal
+    Given a 4 year old "dog" named "Nicki" is registered
+    Then it should have the correct name, type, and age
+    And it should be listed as old
+
+  Scenario: Young animal
+    Given a 3 year old "cat" named "Ollie" is registered
+    Then it should not be listed as old
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,21 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^a (\d+) year old "(.*?)" named "(.*?)" is registered$/) do |age, type, name|
+  age = age.to_i
+  @animal = Animal.new(name, type, age)
+  @name = name
+  @type = type
+  @age = age
+end
+
+Then(/^it should have the correct name, type, and age$/) do
+  expect(@animal.name).to eq @name
+  expect(@animal.type).to eq @type
+  expect(@animal.age).to eq @age
+end
+
+And(/^it should be listed as old$/) do
+  expect(@animal.old?).to eq true
+end
+
+Then(/^it should not be listed as old$/) do
+  expect(@animal.old?).to eq false
+end
