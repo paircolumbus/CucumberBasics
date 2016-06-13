84ca2ea110ae7989383a36e9dd6d0fd23d942b46
Josh Meek <jmeek@covermymeds.com>
2016-06-13 11:01:13 -0400

    complete step defs and feature file for animal

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,15 @@
-#Create your feature here
+Feature: Animal
+  As a Classy Hotel worker
+  Given a client who is an animal
+  I want to track the name, type, age, and if the client is old
+
+Scenario Outline: Animal Creation
+  Given a <animal_age> year old <animal_type> name <animal_name>
+  Then the animal is <old>
+  And the name, age, and type should match
+
+Examples:
+  | animal_age | animal_type | animal_age  | old     |
+  | Harry      | cat         | 1           | not old |
+  | Winnie     | dog         | 1           | not old |
+  | Old Yeller | dog         | 10          | old     |
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,16 @@
-#Delete this comment, here is where you should write your step defs
+Given 'a $animal_age year old $animal_type name $animal_name' do |_animal_age, _animal_type, _animal_name|
+  @age = _animal_age.to_i
+  @type = _animal_type
+  @name = _animal_name
+  @animal = Animal.new(_animal_name, _animal_type, _animal_age.to_i)
+end
+
+Then (/^the animal is (old|not old)$/) do |situation|
+  expect(@animal.old?).to be situation == 'old'
+end
+
+And 'the name, age, and type should match' do
+  expect(@animal.name).to be @name
+  expect(@animal.type).to be @type
+  expect(@animal.age).to be @age
+end
