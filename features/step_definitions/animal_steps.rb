9bd4d5522edbd409b0ef163e170ea6089bce3d46
drm2 <davidrmyersii@gmail.com>
2016-11-23 14:48:59 -0500

    Adding feature and step defs files.

--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,18 @@
 #Delete this comment, here is where you should write your step defs
+
+Given(/^a (\w+) named (\w+) that is (\d+) years old$/) do |type, name, age|
+  @animal = Animal.new(name, type, age.to_i)
+end
+
+When(/^the (\w+) ([\w']+) older than (\d+)$/) do |type, verb, age|
+  is_old = (verb == "is")
+
+  expect(@animal.old?).to eq(is_old)
+end
+
+Then(/^the (\w+) ([\w']+) old$/) do |type, verb|
+  is_old = (verb == "is")
+
+  expect(@animal.type).to eq(type)
+  expect(@animal.old?).to eq(is_old)
+end
