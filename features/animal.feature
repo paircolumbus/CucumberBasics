e563571388c32f5e0741f6315c5623bcb0cbdbf8
Erik Thiem <ethiem@covermymeds.com>
2017-05-22 13:38:28 -0400

    Added animal features and steps

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,22 @@
-#Create your feature here
+Feature: Animal
+  Animals should properly return their attributes.
+
+  Scenario: Name
+    Given an animal named Lucy
+    Then the animal should be named Lucy
+
+  Scenario: Type
+    Given an animal that is a cat
+    Then the animal should be a cat
+
+  Scenario: Age
+    Given an animal that is 3 years old
+    Then the animal should be 3 years old
+
+  Scenario: Old
+    Given an animal that is 5 years old
+    Then the animal should be old
+
+  Scenario: Young
+    Given an animal that is 2 years old
+    Then the animal should be young
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,31 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^an animal named (.*?)$/) do |name|
+  @animal = Animal.new(name, 'cat', 4)
+end
+
+Given(/^an animal that is a (.*?)$/) do |type|
+  @animal = Animal.new('george', type, 4)
+end
+
+Given(/^an animal that is (\d+) years old$/) do |age|
+  @animal = Animal.new('george', 'cat', age.to_i)
+end
+
+Then(/^the animal should be a (.*?)$/) do |type|
+  expect(@animal.type).to eq type
+end
+
+Then(/^the animal should be named (.*?)$/) do |name|
+  expect(@animal.name).to eq name
+end
+
+Then(/^the animal should be (\d+) years old$/) do |age|
+  expect(@animal.age).to eq age.to_i
+end
+
+Then(/^the animal should be old$/) do
+  expect(@animal.old?).to be true
+end
+
+Then(/^the animal should be young$/) do
+  expect(@animal.old?).to be false
+end
