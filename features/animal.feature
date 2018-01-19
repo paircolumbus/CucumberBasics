4bcf87447b406adb4ee64e971958a4e1353a149d
David Hatten <dhatten@cmmc02vt0hzg8wl.innova.local>
2018-01-19 16:30:33 -0500

    added animal steps and feature

--- features/animal.feature
+++ features/animal.feature
@@ -1,1 +1,11 @@
-#Create your feature here
+Feature: Animal
+  In order to be a complete animal
+  I must have a name, type, and age
+
+  Scenario: Dog
+    Given a 12 year old Dog named Bruce
+    Then they should be old
+    And their species should be Dog
+    And their name should be Bruce
+
+
--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,15 @@
-#Delete this comment, here is where you should write your step defs
+Given(/^a (\d+) year old (\w+) named (\w+)$/) { |age, breed, name|
+  @animal = Animal.new(name, breed, Integer(age))
+}
+
+Then(/^they should be old$/) {
+  expect(@animal.old?).to be true
+}
+
+And(/^their species should be (\w+)$/) { |species|
+  expect(@animal.type).to eq species
+}
+
+And(/^their name should be (\w+)$/) { |name|
+  expect(@animal.name).to eq name
+}
