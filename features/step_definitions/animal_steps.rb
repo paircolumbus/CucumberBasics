acd1a0ecb28472c2cfc2508342ed1d304ead001b
Andrew Bonner <abonner@covermymeds.com>
2018-05-11 14:31:23 -0400

    Stub out steps.

--- features/step_definitions/animal_steps.rb
+++ features/step_definitions/animal_steps.rb
@@ -1,1 +1,15 @@
+Given(/^an animal that is four years old$/) do
+  pending # express the regexp above with the code you wish you had
+end
 
+Then(/^it is named 'Gidget'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it has a type of 'cat'$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^it is considered old$/) do
+  pending # express the regexp above with the code you wish you had
+end
